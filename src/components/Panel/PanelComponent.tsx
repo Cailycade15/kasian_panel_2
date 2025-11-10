3495d31a1e3939d50c2be819fffb2d6570fdf188
Cailycade <pencovigor@gmail.com>
2025-11-10 15:52:31 +0300

    Start code

---
+++ src/components/Panel/PanelComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Panel.css";
+
+function PanelComponent() {
+  return (
+    <div className="box">
+      
+    </div>
+  );
+}
+
+export default PanelComponent;
